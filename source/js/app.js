8f0dcf57674bccb8a84ca2aa3e6eac88f473909d
Alexander Nesvit <nesvit.alexander@gmail.com>
2019-01-14 22:42:01 +0300

    create OPS application

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,22 @@
 const GoogleMap = require('./components/api.google');
 const config = require('./config.json');
 const DOM = require('./components/dom');
-
-
+const OPS = require('./components/ops');
 const DOM_API = new DOM();
+
+
+//OPS
+const ops = new OPS('.maincontent', {
+    events: {
+        keyup: true
+    },
+    controls: ['.pagination'],
+    duration: 1000,
+    animate: 'cubic-bezier(0.075, 0.82, 0.165, 1)'
+});
+/////////////
+
+//map
 const myApiGoogle = new GoogleMap();
 
 myApiGoogle.init('#map', {
@@ -18,8 +31,11 @@ config.googleMap.markers.forEach(coords => {
         DOM_API.showPopup('#pupup-status', data.address);
     });
 });
+////////////
 
+//Jquery scripts
 $(document).ready(function () {
+    //Slider
     $(".owl-carousel").owlCarousel({
         items: 1,
         loop: true,
@@ -34,4 +50,12 @@ $(document).ready(function () {
             "<svg class=\"slider__control-icon\"><use xlink:href=\"images/icons/sprite.svg#arrow-scroll\" /></svg>"
         ]
     });
+    ////////////
+
+    //
+    $('.nav__link, .button_action_scroll, .arrow-down').on('click', function () {
+        const index = this.getAttribute('href').substring(1);
+        ops.slideTo(parseInt(index));
+    });
 });
+///////////
---
+++ source/js/components/ops.js
@@ -0,0 +1,113 @@
+module.exports = class {
+    constructor(selector, settings) {
+        this.container = document.querySelector(selector);
+        this.sections = this.container.children;
+        this.settings = settings;
+        this.posIndex = 0;
+        this.isAnimate = false;
+
+        window.addEventListener('wheel', this.handlers.bind(this));
+
+        for (const event in settings.events) {
+            if (settings.events.hasOwnProperty) {
+
+                if (settings.events[event]) {
+                    window.addEventListener(event, this.handlers.bind(this));
+                }
+            }
+        }
+
+        settings.controls.forEach(selector => {
+            const nav = document.querySelector(selector);
+
+            nav.innerHTML = require(`../../view/components/template-pagination.hbs`)({
+                root: selector.substring(1),
+                sections: this.sections
+            });
+
+            this.reactive(0);
+
+            nav.addEventListener('click', this.handlers.bind(this));
+        });
+    }
+
+    handlers(e) {
+        if (this.isAnimate) return;
+
+        switch (e.type) {
+            case 'wheel':
+                if (e.deltaY > 0) {
+                    this.updatePos('down');
+                } else {
+                    this.updatePos('up');
+                }
+                break;
+            case 'keyup':
+                if (e.keyCode === 40) {
+                    this.updatePos('down');
+                } else if (e.keyCode === 38) {
+                    this.updatePos('up');
+                }
+                break;
+            case 'click':
+                if (e.target.tagName === 'A') {
+                    const index = e.target.getAttribute('href').substring(1);
+
+                    this.reactive(index);
+                    this.updatePos(index);
+                }
+                break;
+        }
+    }
+
+    reactive(index) {
+        this.settings.controls.forEach((selector) => {
+            const nav = document.querySelector(selector);
+            const list = nav.firstElementChild;
+            const items = list.children;
+
+            for (const item of items) {
+                item.classList.remove('active');
+            }
+
+            items[parseInt(index)].classList.add('active');
+        });
+    }
+
+    updatePos(vector) {
+        switch (vector) {
+            case 'down':
+                if (this.sections[this.posIndex + 1]) {
+                    this.slideTo(++this.posIndex);
+                }
+                break;
+            case 'up':
+                if (this.sections[this.posIndex - 1]) {
+                    this.slideTo(--this.posIndex);
+                }
+                break;
+            default:
+                this.posIndex = parseInt(vector);
+                this.slideTo(this.posIndex);
+
+        }
+    }
+
+    slideTo(index) {
+        if (this.sections[index]) {
+            this.isAnimate = true;
+            this.posIndex = index;
+
+            this.container.style.setProperty("--transformY", `${-index * 100}%`);
+            this.container.style.setProperty("--transition-slide", `${this.settings.duration || 1}ms ${this.settings.animate}`);
+
+            this.reactive(index);
+
+            setTimeout(() => {
+                this.isAnimate = false;
+            }, this.settings.duration);
+        } else {
+            throw new Error(`the section at this index is not found`);
+        }
+    }
+}
